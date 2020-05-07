bcdfadac4ef6ab39a20af9ac863e5bc7f88adee9
Eduardo Silva <evinicius16@gmail.com>
2020-05-06 23:51:24 -0300

    criado esqueleto tela relatorio

---
+++ src/componentes/relatorio/relatorio.js
@@ -0,0 +1,24 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var carousel = document.querySelectorAll('.carousel');
+    var carouselOptions = {
+        fullWidth: true,
+        indicators: true,
+        noWrap: true
+    }
+    var selects = document.querySelectorAll('select');
+    var selectsOptions = {};
+
+    var elems = document.querySelectorAll('.sidenav');
+    
+    M.Sidenav.init(elems);
+    M.FormSelect.init(selects, selectsOptions);
+    M.Carousel.init(carousel, carouselOptions);
+  });
+
+var lm = new Vue({
+    el:"#Calorias",
+    data: {
+        
+    }
+})
