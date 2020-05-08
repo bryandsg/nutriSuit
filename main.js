28b6bd57e36fca4a91eac99370f07c928a93eb52
Bryan Gomes <bryandossantosg8@gmail.com>
2020-05-08 08:22:42 -0300

    organizando os links do projeto

--- main.js
+++ main.js
@@ -2,5 +2,3 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     M.Sidenav.init(elems);
   });
-
-  import { Bar } from 'vue-chartjs';
